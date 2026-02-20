a3cfe127d4a34d6e0e1fd0234012121b6592cae1
Fin-ABC <Ahmad.snesas@gmail.com>
2026-02-21 00:16:11 +0700

    latihan menanam benih

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            'name' => 'Fin',
+            'email' => 'Fin@nesas.com',
+            'password' => Hash::make('password'),
+            'remember_token' => Str::random(10),
+            'name' => 'Haru',
+            'email' => 'Haru@nesas.com',
+            'password' => Hash::make('password'),
+            'remember_token' => Str::random(10),
+        ]);
+    }
+}
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -166,7 +166,7 @@
                     <a href="contact" class="text-sm/6 font-semibold text-white">Contact</a>
                 </el-popover-group>
                 <div class="hidden lg:flex lg:flex-1 lg:justify-end">
-                    <a href="#" class="text-sm/6 font-semibold text-white">Log in <span
+                    <a href="fin" class="text-sm/6 font-semibold text-white">Atmin <span
                             aria-hidden="true">&rarr;</span></a>
                 </div>
             </nav>
