79f28864e6c93da2013a22403e66e0c6a3e78215
Fin-ABC <ahmad.snesas@gmail.com>
2026-02-25 15:00:28 +0700

    coba buat table

--- app/Filament/Resources/Students/Tables/StudentsTable.php
+++ app/Filament/Resources/Students/Tables/StudentsTable.php
@@ -5,6 +5,8 @@ namespace App\Filament\Resources\Students\Tables;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Table;
 
 class StudentsTable
@@ -13,10 +15,29 @@ class StudentsTable
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('nis')->label('NIS'),
+                TextColumn::make('name')->label('Nama')->sortable(),
+                TextColumn::make('gender')->label('Gender'),
+                TextColumn::make('majors.name_major')->label('Jurusan'),
+                TextColumn::make('date_of_birth')->label('Tanggal Lahir'),
+                TextColumn::make('address')->label('Alamat'),
             ])
             ->filters([
-                //
+                SelectFilter::make('gender')->options([
+                    'l' => 'Laki-laki',
+                    'p' => 'Perempuan',
+                ])
+            ])
+            ->pushColumns([
+                TextColumn::make('created_at')
+                    ->label('Created')
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+
+                TextColumn::make('updated_at')
+                    ->label('Updated')
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
             ])
             ->recordActions([
                 EditAction::make(),
